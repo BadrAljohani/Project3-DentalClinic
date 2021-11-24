c785c8c55ff8d6eca17a4b3fef35f894fa211bb8
KhalidAli12 <91524742+KhalidAli12@users.noreply.github.com>
2021-11-24 11:14:23 +0300

    Co-author: Yousef (#1)
    Co-author: Badr
    add the detail patients in table view

---
+++ Project3#Try/PatientResult.swift
@@ -0,0 +1,29 @@
+//
+//  PatientResult.swift
+//  DentalClinic
+//
+//  Created by khalid ali on 19/04/1443 AH.
+//
+
+import UIKit
+
+class PatientResult: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
